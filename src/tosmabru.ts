2d0fe5e0d6a8603686098763407a5481cc69c9f5
skytomo <tomo1kawa.fullerene@gmail.com>
2022-02-11 11:41:45 +0900

    Refactor tosmabru

--- src/tosmabru.ts
+++ src/tosmabru.ts
@@ -1,30 +1,37 @@
 import { bigram } from 'n-gram';
 import {
-  Consonant, isCmevla, isPermissible, syllables,
+  Consonant, isCmevla, isConsonant, isPermissible, syllables,
 } from './tools';
 
+function cc(r1: string, r2: string): boolean {
+  return (
+    isConsonant(r1[r1.length - 1])
+    && isConsonant(r2[0])
+    && isPermissible(r1[r1.length - 1] as Consonant, r2[0]) === 2
+  );
+}
+
 export default function tosmabru(rafsis: string[]): boolean {
-  const heads = rafsis.includes('y')
-    ? rafsis.slice(0, rafsis.indexOf('y'))
-    : rafsis.slice(0, -1);
   const last = rafsis[rafsis.length - 1];
   if (rafsis.length < 2 || isCmevla(last)) return false;
-  if (heads.every((rafsi) => syllables(rafsi) === 'CVC')) {
-    if (rafsis.includes('y')) {
-      return bigram(heads).every(
-        ([r1, r2]) => isPermissible(r1[r1.length - 1] as Consonant, r2[0] as Consonant)
-          === 2,
-      );
-    }
-    if (
-      syllables(last) === 'CVCCV'
-      && isPermissible(last[2] as Consonant, last[3] as Consonant) === 2
-    ) {
-      return bigram(rafsis).every(
-        ([r1, r2]) => isPermissible(r1[r1.length - 1] as Consonant, r2[0] as Consonant)
-          === 2,
-      );
-    }
+  if (rafsis.includes('y')) {
+    const heads = rafsis.slice(0, rafsis.indexOf('y'));
+    return (
+      heads.every((rafsi) => syllables(rafsi) === 'CVC')
+      && bigram(heads).every(([r1, r2]) => cc(r1, r2))
+    );
+  }
+  if (
+    syllables(last) === 'CVCCV'
+    && isConsonant(last[2])
+    && isConsonant(last[3])
+    && isPermissible(last[2], last[3]) === 2
+  ) {
+    const heads = rafsis.slice(0, -1);
+    return (
+      heads.every((rafsi) => syllables(rafsi) === 'CVC')
+      && bigram(rafsis).every(([r1, r2]) => cc(r1, r2))
+    );
   }
   return false;
 }
